b24b95be90ad78488afa84a648a864463b7b7634
rcoridan <coridan@gmail.com>
2014-05-16 23:26:19 -0700

    d3.js Plots of NSF data

--- 20140515_nsfAwards_feature.py
+++ 20140515_nsfAwards_feature.py
@@ -1,8 +1,15 @@
 import pandas as pd
-filename ='2012nsf_awards.csv'
-df=pd.read_csv(filename)
+import os, glob
 
-keepList=['AbstractNarration','AwardAmount','AwardEffectiveDate','AwardExpirationDate','AwardID','AwardTitle','CityName','CountryName','LongName','LongName1','Name','StateCode','StateName','Text','Text1','year']
+filelist=glob.glob('201*nsf_awards.csv')
+coll=pd.DataFrame()
+for filename in filelist:
+	df=pd.read_csv(filename)
 
-df=df[keepList]
-df.to_csv('2012_clean_nsf_awards.csv')
+	keepList=['AbstractNarration','AwardAmount','AwardEffectiveDate','AwardExpirationDate','AwardID','AwardTitle','CityName','CountryName','LongName','LongName1','Name','StateCode','StateName','Text','Text1','year']
+
+	df=df[keepList]
+	df.to_csv('clean_'+filename)
+	coll=pd.concat([coll,df],ignore_index=True)
+coll.to_csv('clean_nsf_awards_all.csv')
+print len(coll)
---
+++ 20140516_NSF_201_ex.r
@@ -0,0 +1,63 @@
+
+require(devtools)
+require(rCharts)
+#install_github('slidify','ramnathv',ref='dev')
+#install_github('slidifyLibraries','ramnathv',ref='dev')
+options(RCHART_TEMPLATE = 'Rickshaw.html')
+
+
+#get data
+df<-read.csv('clean_nsf_awards_all.csv')
+directorates<-unique(unlist(df$LongName))
+years<-unique(unlist(df$year))
+
+cc=data.frame()
+
+for (yr in years){
+  for (dd in directorates){
+    num<-length(df[  df$LongName == dd & df$year == yr ,'year'])
+    totExp<-sum(df[  df$LongName == dd & df$year == yr,'AwardAmount'])
+    
+    #fix some strings
+    dnm<-dd
+    if (dd == ""){
+      dnm<-'Not Available'
+    }
+    if (dd == directorates[5]){
+      # "OFFICE OF THE DIRECTOR"
+      dnm<-'Office of the Director'
+    }
+    
+    #save points to collector dataframe cc
+    #yrdate<-as.Date(toString(yr),format='%Y')
+    yrdate<-ISOdate(yr,1,1,12,0,0)
+    exi<-data.frame(Year=yrdate,Directorate=dnm,TotGrants=num,TotExp=totExp,stringsAsFactors=FALSE)
+    cc=rbind(cc,exi)
+  }
+}
+
+
+r5 <- Rickshaw$new()
+r5$layer ( 
+  TotGrants ~ Year, data = cc,groups = "Directorate",
+  height = 600,width = 800
+)
+#turn off features not used in the example
+r5$set(
+  hoverDetail = TRUE, shelving = FALSE, legend = TRUE,
+  slider = FALSE, highlight = FALSE, scheme='colorwheel'
+)
+r5
+
+
+r6 <- Rickshaw$new()
+r6$layer ( 
+  TotExp ~ Year, data = cc,groups = "Directorate",
+  height = 600,width = 800
+)
+#turn off features not used in the example
+r6$set(
+  hoverDetail = TRUE, shelving = FALSE, legend = TRUE,
+  slider = FALSE, highlight = FALSE, scheme='colorwheel'
+)
+r6
